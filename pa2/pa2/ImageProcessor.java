97ff573b45325d31ec5c611dedfe6db3eb094fe6
Merin Mundt <merin@iastate.edu>
2019-12-11 22:51:08 -0600

    image processor

--- pa2/pa2/ImageProcessor.java
+++ pa2/pa2/ImageProcessor.java
@@ -1,17 +1,38 @@
 package pa2;
 
+import java.util.*;
 
 public class ImageProcessor{
     static Picture reduceWidth(int x, String inputImage){
         Picture pic = new Picture(inputImage);
-        int i = 0;
-        while(i < x) {
+        int n = 0;
+        //int[][] I = new int[][];
+        while(n < x) {
+        	//get the imporance array from the importance array method
+        	int[][] I = getImportance(pic);
+        	Picture newPic = new Picture(pic.width() - 1, pic.height());
+        	ArrayList<Tuple> minCost = new ArrayList<Tuple>();
+        	minCost.addAll(MatrixCuts.widthCut(I));
         	
+        	
+        	//setting the pic to its new pic without the pixels that have been removed
+        	for(int i = 0; i < minCost.size(); i++) {
+        		for(int j = 0; j < newPic.width(); j++) {
+        			if(j < minCost.get(i).getY()) {
+        				newPic.set(j, i, pic.get(j, i));
+        			}
+        			else if(j >= minCost.get(i).getY()) {
+        				newPic.set(j+1, i, pic.get(j+1, i));
+        			}
+        		}
+        	}
+        	pic = newPic;
         }
         
         return pic;
     	
     }
+    
     static int[][] getImportance(Picture image){
     	int imageHeight = image.height();
     	int imageWidth = image.width();
@@ -26,6 +47,9 @@ public class ImageProcessor{
     			}
     			if(j == 0) {
     				importanceArr[i][j] = ImageStitch.pixelDistance(image.get(j, i), image.get(j+1, i));
+    			}
+    			if(j == imageWidth -1) {
+    				importanceArr[i][j] = ImageStitch.pixelDistance(image.get(j, i), image.get(j-1, i));
     			}
     		}
     	}
