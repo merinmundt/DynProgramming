cd148e2dc91fe0bb34e854f8571d1926f899ef3b
Merin Mundt <merin@iastate.edu>
2019-12-11 21:11:15 -0600

    now were moving

--- pa2/pa2/ImageProcessor.java
+++ pa2/pa2/ImageProcessor.java
@@ -4,7 +4,31 @@ package pa2;
 public class ImageProcessor{
     static Picture reduceWidth(int x, String inputImage){
         Picture pic = new Picture(inputImage);
+        int i = 0;
+        while(i < x) {
+        	
+        }
+        
         return pic;
     	
+    }
+    static int[][] getImportance(Picture image){
+    	int imageHeight = image.height();
+    	int imageWidth = image.width();
+    	int[][] importanceArr = new int[imageWidth][imageHeight];
+    	
+    	//getting the array for each (i,j) based on the importance algorithms given on the assignmnet pdf
+    	for(int i = 0; i < imageHeight; i++) {
+    		
+    		for(int j = 0; j < imageWidth; j++) {
+    			if((j > 0) && (j < imageWidth - 1)) {
+    				importanceArr[i][j] = ImageStitch.pixelDistance(image.get(j-1, i), image.get(j+1, i));
+    			}
+    			if(j == 0) {
+    				importanceArr[i][j] = ImageStitch.pixelDistance(image.get(j, i), image.get(j+1, i));
+    			}
+    		}
+    	}
+    	return importanceArr;
     }
 }
--- pa2/pa2/MatrixCuts.java
+++ pa2/pa2/MatrixCuts.java
@@ -1,129 +1,175 @@
 package pa2;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Stack;
+import java.io.*;
+import java.util.*;
 
-import java.io.*; 
-import java.util.*; 
 
-public class MatrixCuts{
+public class MatrixCuts {
 
-    static ArrayList<Tuple> widthCut(int[][] M){
-    	ArrayList<Tuple> minCost = new ArrayList<Tuple>();
-//    	Tuple[] answers;
-//    	for (int i = 0; i < M.length; i++) {
-//    		//add first tuple to list?
-//    		for(int j = 0; j < 3; j++) {
-//    			//check the next three options
-//    			
-//    		}
-//    	}
-    	
-    	
-    	
-    	
-    	//all of this is before I actually knew what I was doing. 
-    	int cost = 0;
-    	int lowest = M[0][0];
-    	//Tuple curr = new Tuple(0,0);
-    	int x = 0;
-    	int y = 0;
-    	for(int i = 0; i < M[0].length; i++) {
-    		int num = M[0][i];
-    		if(num < lowest) {
-    			lowest = num;
-    			x = 0;
-    			y = i;
-    		}
-    	}
-    	//first add the tuple that will be in place for the cost of the final path
-    	//will get updated at the end with the ending cost
-    	cost = cost + M[x][y];
-    	minCost.add(new Tuple(x,y));
-    	
-    	for(int j = 0; j < M.length; j++) {
-    		int min = 0; 
-    		int min2 = 0;
-    		int min3 = 0;
-    		
-    		//need to check if valid**
-    		if((y-1 >= 0) && (x+1 <= M.length-1)) {
-        		min = M[x+1][y-1];
-    		}
-    		else {
-    			//moving this way is not a valid move
-    			//therefore set min to be very very large
-    			min = 10000;
-    		}
-    		//need to check if valid**
-    		if(x+1 <= M.length-1) {
-    			min2 = M[x+1][y];
-    		}
-    		else {
-    			//moving this way is not a valid move
-    			//therefore set min to be very very large
-    			min2 = 10000;
-    		}
-    		//need to check if valid**
-    		if(y+1 <= M[j].length) {
-    			min3 = M[x+1][y+1];
-    		}
-    		else {
-    			//moving this way is not a valid move
-    			//therefore set min to be very very large
-    			min2 = 10000;
-    		}
-    		
-    		//finding the best move
-    		if(min < min2) {
-    			if(min < min3) {
-    				x = x+1;
-    				y = y-1;
-    			}
-    			else {
-    				x = x+1;
-    				y = y+1;
-    			}
-    		}
-    		else {
-    			if(min2 < min3) {
-    				x = x+1;
-    			}
-    			else {
-    				x = x+1;
-    				y = y+1;
-    			}
-    		}
-    	
-    		minCost.add(new Tuple(x,y));
-    		cost = cost + M[x][y];
-    		
-    	}
-    	
-    		
-    	minCost.add(0, new Tuple(cost, -1));
-    	return minCost;
+
+    public static ArrayList<Tuple> widthCut(int [][] M){
+        //To store the result after cutting.
+        ArrayList<Tuple> result = new ArrayList<Tuple>();
+
+        //If M is invalid, return our empty result
+        if(M == null || M.length == 0) return result;
+
+        int rows = M.length;
+        int cols = M[0].length;
+
+        int [][] DP = new int [rows][cols];
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(i == 0){
+                    DP[i][j] = M[i][j]; // We can only get to the first row from the first row.
+                }else if(j == 0){
+                    DP[i][j] = DP[i - 1][j] + M[i][j]; //We can only get to the first column from the elements abouve
+                }else if(j + 1 < cols ){
+                    DP[i][j] = M[i][j] +  min(DP[i-1][j], DP[i-1][j+1], DP[i-1][j-1]);  //We can get to the mid column from 3 places.
+                }else{
+                    DP[i][j] = M[i][j] + Math.min(DP[i-1][j], DP[i-1][j-1]); //We can only get to the last column from two places
+                }
+            }
+        }
+
+        //By the time we are done, the min cost will be the cell with the smallest value in the last row.
+
+        int i = rows - 1;
+        int min_col = 0;
+        int min_cost = DP[i][min_col]; //Start with the min first element in the last row.
+
+        //We will find the min element in the last row.
+        for(int a = 0; a < cols; a++){
+            if(DP[i][a] < min_cost){
+                min_cost = DP[i][a];
+                min_col = a;
+            }
+        }
+
+        //Add the first (last pos)
+        result.add(new Tuple(i, min_col));
+
+
+        int tempIndex = 0;
+        int row_min;
+
+        //Perculate back and try to figure out how we get back up. We have 3 choices
+        for(int b = rows - 2; b >= 0; b--){
+            row_min = Integer.MAX_VALUE;
+
+            if(min_col + 1 < cols){
+                row_min = DP[b][min_col + 1];
+                tempIndex = min_col + 1;
+            }
+
+            if(min_col - 1 >= 0){
+                if(row_min > DP[b][min_col - 1]){
+                    row_min = DP[b][min_col - 1];
+                    tempIndex = min_col - 1;
+                }
+            }
+
+            if(row_min > DP[b][min_col]){
+                row_min = DP[b][min_col];
+                tempIndex = min_col;
+            }
+
+            result.add(new Tuple(b, tempIndex));
+            min_col = tempIndex;
+        }
+
+        result.add(new Tuple(min_cost, -1));
+        Collections.reverse(result);
+        return result;
+
     }
-    	
-    static ArrayList<Tuple> stitchCut(int[][] M){
-    	ArrayList<Tuple> minCost = new ArrayList<Tuple>();
-    	int cost = 0;
-    	minCost.add(new Tuple(0,0));
-    	cost = cost + M[0][0];
-    	int worstCase = M.length * M[M.length].length;
-    	for (int i = 0; i < worstCase; i++) {
-    		int min = 0; 
-    		int min2 = 0;
-    		int min3 = 0;
-    	}
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	return minCost;
+
+
+    public static ArrayList<Tuple> stitchCut(int [][] M){
+
+        ArrayList<Tuple> result = new ArrayList<Tuple>();
+
+        //If M is invalid, return our empty result
+        if(M == null || M.length == 0) return result;
+
+        int rows = M.length;
+        int cols = M[0].length;
+
+        int [][] DP = new int [rows][cols];
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(i == 0){
+                    DP[i][j] = M[i][j]; //First Row
+                }else if(j == 0){
+                    DP[i][j] = DP[i - 1][j] + M[i][j]; //We can only get to the first column from cells above
+                }else{
+                    DP[i][j] = M[i][j] +  min(DP[i-1][j-1], DP[i-1][j], DP[i][j-1]); //We can get to the cells in the middle from min of the 3 spots.
+                }
+            }
+        }
+
+        //By the time we are done, the min cost will be the cell with the smallest value in the last row.
+
+        int i = rows - 1;
+        int min_col = 0;
+        int min_cost = DP[i][0]; //Start with the min first element in the last row.
+
+        //We will find the min element in the last row.
+        for(int a = 0; a < cols; a++){
+            if(DP[i][a] < min_cost){
+                min_cost = DP[i][a];
+                min_col = a;
+            }
+        }
+
+        //Add the first one.
+        result.add(new Tuple(i, min_col));
+
+        //Now we perculate back up to find the min cost path
+        while(i > 0){
+            if(min_col > 0 && DP[i][min_col - 1] < DP[i - 1][min_col] && DP[i][min_col - 1] < DP[i - 1][min_col - 1]){
+                result.add(new Tuple(i, min_col - 1));
+                min_col--;
+            }else if(min_col == 0 || i > 0 && DP[i - 1][min_col] < DP[i][min_col - 1] && DP[i - 1][min_col] < DP[i - 1][min_col - 1]){
+                result.add(new Tuple(i - 1, min_col));
+                i--;
+            }else if(i > 0 && min_col > 0){
+                result.add(new Tuple(i - 1, min_col - 1));
+                i--;
+                min_col--;
+            }
+        }
+        //Add the cost tuple to the ArrayList.
+        result.add(new Tuple(min_cost, -1));
+        Collections.reverse(result);
+        return result;
+
     }
+
+    private static int min(int a, int b, int c){
+        return Math.min(a, Math.min(b,c));
+    }
+    public static void main(String[] args) {
+       int [][] M = {{5,7,9,4,5},{2,3,1,1,2},{2,0,49,46,8},{3,1,1,1,1},{50,51,25,26,1}};
+
+       ArrayList<Tuple> s = widthCut(M);
+       ArrayList<Tuple> e = stitchCut(M);
+        System.out.println("WidthCut");
+        for(Tuple t : s){
+           System.out.println(t.toString());
+       }
+
+       System.out.println("---------------");
+        System.out.println("Stich Cut");
+
+        for(Tuple ta : e){
+            System.out.println(ta.toString());
+        }
+
+    }
+
 }
